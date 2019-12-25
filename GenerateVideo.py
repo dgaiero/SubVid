bd0f79d8c4e16a57201358b4737c82b8e58fec0a
Dominic Gaiero <dpgaiero@gmail.com>
2019-12-25 15:31:19 -0800

    update add audio with ffmpeg

--- GenerateVideo.py
+++ GenerateVideo.py
@@ -1,49 +1,71 @@
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, pyqtSignal
 import cv2
 import moviepy.editor as mpe
+from moviepy.editor import ColorClip
 import numpy
 from AppParameters import Settings
 from draw_background import convert_to_qt, draw_frame
+import os
+import subprocess
+import tempfile
 
 
 class GenerateVideo(QThread):
+
+   text: pyqtSignal = pyqtSignal(object)
+   image: pyqtSignal = pyqtSignal(object)
+   fail: pyqtSignal = pyqtSignal()
+   update : pyqtSignal= pyqtSignal()
 
    def __init__(self):
       QThread.__init__(self)
       self.settings: Settings
 
-   def __del__(self):
-      self.wait()
+   # def __del__(self):
+   #    self.wait()
 
    def generateVideo(self):
+      tempFile = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
+      tempFile.close()
+      print(tempFile.name)
       pilImg: Image = draw_frame(
           self.settings, self.settings.frameTextList[0].line)
       width, height = pilImg.size
       print(f"({width}, {height})")
       # Create the OpenCV VideoWriter
-      video: cv2.VideoWriter = cv2.VideoWriter(self.settings.output_location,  # Filename
-         -1,  # Negative 1 denotes manual codec selection. You can make this automatic by defining the "fourcc codec" with "cv2.VideoWriter_fourcc"
-         # 10 frames per second is chosen as a demo, 30FPS and 60FPS is more typical for a YouTube video
+      video: cv2.VideoWriter = cv2.VideoWriter(tempFile.name,
+                                               -1,
          self.settings.framerate,
-         # The width and height come from the stats of image1
          (width, height)
          )
       for i in range(len(self.settings.frameTextList)):
-         print(self.settings.frameTextList[i].line)
+         # print(self.settings.frameTextList[i].line)
+         # self.emit(pyqtSignal('add_post(QString)'),
+         #           self.settings.frameTextList[i].line)
+         self.text.emit(f"Processing: {self.settings.frameTextList[i].line}")
          pilImg: Image = draw_frame(
              self.settings, self.settings.frameTextList[i].line)
+         self.image.emit(pilImg)
          # frame_length: int = self.settings.framerate*self.settings.frameTextList[i].delta.total_seconds()
          # print(self.settings.frameTextList[i].delta)
          # print(frame_length)
          for i in range(self.settings.frameTextList[i].num_frames):
             video.write(cv2.cvtColor(numpy.array(pilImg), cv2.COLOR_RGB2BGR))
+         self.update.emit()
       video.release()
-      vclp = mpe.VideoFileClip(self.settings.output_location)
-      audio_background = mpe.AudioFileClip(self.settings.sound_track)
-      audio_background.set_fps(float(self.settings.framerate))
-      # final_audio = mpe.CompositeAudioClip([vclp.audio, audio_background])
-      final_vclp = vclp.set_audio(audio_background)
-      final_vclp.write_videofile(self.settings.output_location, fps=float(self.settings.framerate))
+      self.text.emit('Writing audio to file')
+      vclp = addAudioToVideo(tempFile.name, self.settings.sound_track,
+         self.settings.output_location)
+      if vclp != 0:
+         self.fail.emit()
+      self.update.emit()
+      os.remove(tempFile.name)
 
    def run(self):
       self.generateVideo()
+
+def addAudioToVideo(video, audio, output):
+   spc = subprocess.call(['ffmpeg', '-y', '-i', video, '-i', audio, '-c:v', 'copy',
+                    '-map', '0:v:0', '-map', '1:a:0', '-c:a', 'aac', '-b:a', '192k', output])
+   return spc
+# ffmpeg -i scb2018_2.mp4 -i "King Tut Scrubby_mixdown.wav" -c:v copy -map 0:v:0 -map 1:a:0 -c:a aac -b:a 192k output.mp4
