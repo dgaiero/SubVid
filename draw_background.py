18bacf55c877b5c391ae3ace32a0252cc060a0b6
Dominic Gaiero <dpgaiero@gmail.com>
2019-12-28 19:06:00 -0800

    add two lined mode to image drawing

--- draw_background.py
+++ draw_background.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFont, ImageDraw
 from AppParameters import Settings
 from PyQt5 import QtGui
 
-def draw_frame(appData: Settings, text: str):
+def draw_frame(appData: Settings, text: list):
 
    img = Image.open(appData.background_frame)
    W, H = img.size
@@ -10,8 +10,27 @@ def draw_frame(appData: Settings, text: str):
    # font = ImageFont.truetype(<font-file>, <font-size>)
    font = ImageFont.truetype(appData.font, appData.font_size)
    # draw.text((x, y),"Sample Text",(r,g,b))
-   w, h = draw.textsize(text,font)
-   draw.text(((W-w)/2,(H-h)/2),text,tuple(appData.text_color),font=font)
+   # width, total_height = draw.textsize("abcdefghijklmnopqrstuvwxyz", font)
+   h = font.getsize('hg')[1]
+   list_text_height = h * len(text)
+   # print(list_text_height)
+   # print(h)
+   # for i in range(len(text)):
+   if (len(text) == 2):
+      ctext = text[0]
+      w, cth = draw.textsize(ctext,font)
+      draw.text(((W-w)/2, ((H-h)/2) - h/2),
+                  ctext, tuple(appData.text_color), font=font)
+
+      ctext = text[1]
+      w, cth = draw.textsize(ctext, font)
+      draw.text(((W-w)/2, ((H-h)/2) + h/2),
+               ctext, tuple(appData.text_color), font=font)
+   if (len(text) == 1):
+      ctext = text[0]
+      w, cth = draw.textsize(ctext, font)
+      draw.text(((W-w)/2, ((H-h)/2)),
+               ctext, tuple(appData.text_color), font=font)
    return img
 
 def convert_to_qt(image):
@@ -24,4 +43,11 @@ def convert_to_qt(image):
 if __name__ == "__main__":
    appData = Settings()
    appData.background_frame = 'C:\\Users\\dgaiero\\OneDrive - California Polytechnic State University\\Documents\\projects\\SubVid\\layouts\\image_assets\\background_image_placeholder.jpg'
-   draw_frame(appData, "test text")
+   appData.font_size = 88
+   appData.text_color = [0,42,37]
+   appData.font = "C:\\Users\\dgaiero\\OneDrive - California Polytechnic State University\\Documents\\projects\\Scrubby\\2019\\video\\The Wildeast Clean.ttf"
+   appData.background_frame = "C:\\Users\\dgaiero\\OneDrive - California Polytechnic State University\\Documents\\projects\\Scrubby\\2019\\video\\background.jpg"
+   pilImg = draw_frame(
+       appData, ["test text", "another text"])
+   pilImg.show()
+   pilImg.save("output.jpg")
