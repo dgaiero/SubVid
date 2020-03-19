dde7bd85a49516dfde5032ab9e927f76bb5a57cb
Dominic Gaiero <dpgaiero@gmail.com>
2020-03-19 15:16:25 -0700

    refactor: move file open actions into settings

--- AppParameters.py
+++ AppParameters.py
@@ -1,5 +1,6 @@
 import sys, os
 from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QFileDialog
 
 class Settings():
    def __init__(self, source_time='', sound_track='', font='', font_size=1, background_frame='', preview_frame='', output_location='', text_color=''):
@@ -7,8 +8,8 @@ class Settings():
       self.sound_track = sound_track
       self.font = font
       self.font_size = font_size
-      self.background_frame = ':/placeholders/image_assets/background_image_placeholder.jpg'
-      self.preview_frame = ':/placeholders/image_assets/export_preview_placeholder.jpg'
+      self.background_frame = '://placeholders//image_assets//background_image_placeholder.jpg'
+      self.preview_frame = '://placeholders//image_assets//export_preview_placeholder.jpg'
       self.output_location = output_location
       self.text_color = [0,0,0]
       self.frameNumber = 0
@@ -17,18 +18,33 @@ class Settings():
       self.videoInProgress = False
       self.saveFile = None
 
-   def pickleData(self):
-      return {
-         'source_time': self.source_time,
-         'sound_track': self.sound_track,
-         'font': self.font,
-         'font_size': self.font_size,
-         'background_frame': self.background_frame,
-         'output_location': self.output_location,
-         'text_color': self.text_color
+   def configData(self):
+      r_dict = {
+          'source_time': self.source_time,
+          'sound_track': self.sound_track,
+          'font': self.font,
+          'font_size': self.font_size,
+          'background_frame': self.background_frame,
+          'output_location': self.output_location,
+          'text_color': self.text_color
       }
+      # for key, value in r_dict.items():
+      #    print(f"{key}={value} - {type(value)}")
+      return r_dict
 
-   def loadFromPickle(self, source_time='', sound_track='', font='', font_size='', background_frame='', output_location='', text_color=''):
+   def checkFileExists(self, parameter):
+      return os.path.isfile(eval(f"self.{parameter}"))
+
+   def getFileName(self, parameter):
+      return os.path.basename(eval(f"self.{parameter}"))
+
+   def initLine(self, parameter):
+      defaultValue = "''"
+      if parameter == 'background_frame':
+         defaultValue = '://placeholders//image_assets//background_image_placeholder.jpg'
+      exec(f"self.{parameter} = {defaultValue}")
+
+   def loadFromConfig(self, source_time='', sound_track='', font='', font_size='', background_frame='', output_location='', text_color=''):
       self.source_time = source_time
       self.sound_track = sound_track
       self.font = font
@@ -53,3 +69,39 @@ class Settings():
          return False
       else:
          return not(self.output_location == '')
+
+   def getSourceTime(self, parent, openLocation):
+      filters = 'All Acceptable Formats (*.xlsx *.xls *.csv *.tsv);;\
+         Excel Files (*.xlsx *.xls);;\
+         CSV Files (*.csv);;\
+         TSV Files (*.tsv);;\
+         All Files (*.*)'
+      fname = QFileDialog.getOpenFileName(parent, 'Select Timecode File', openLocation,
+                                          filters)
+      return fname
+
+   def getParamFont(self, parent, openLocation, openLocationOverride = False):
+      filters = 'All Acceptable Formats (*.ttf *.otf);;\
+         All Files (*.*)'
+      file_open_dialog_directory = openLocation
+      if not(openLocationOverride):
+         if sys.platform == 'win32':
+            file_open_dialog_directory = '\\\\localhost\\c$\\windows\\fonts'
+         elif sys.platform == 'darwin':
+            file_open_dialog_directory = '~/Library/Fonts'
+      fname = QFileDialog.getOpenFileName(parent, 'Select Font File', file_open_dialog_directory,
+                                          filters)
+      return fname
+
+   def getSoundTrack(self, parent, openLocation):
+      filters = 'All Acceptable Formats (*.mp3 *.m4a *.wav  *.aac *.aif *.aiff);;\
+         All Files (*.*)'
+      fname = QFileDialog.getOpenFileName(parent, 'Select Soundtrack File', openLocation,
+                                          filters)
+      return fname
+
+   def getBackgroundImage(self, parent, openLocation):
+      filters = 'Image files (*.jpg *.jpeg *.gif *.png)'
+      fname = QFileDialog.getOpenFileName(parent, 'Select Background File', openLocation,
+         filters)
+      return fname
