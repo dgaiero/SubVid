736db567edaf8063ca7927c49377323e031bb4b0
Dominic Gaiero <dpgaiero@gmail.com>
2021-12-27 09:28:37 -0800

    update version and add installer

--- MakeInstaller.py
+++ MakeInstaller.py
@@ -3,11 +3,24 @@ from pathlib import Path
 from MakeExecutable import main as MakeExecutableFile
 from version import Version
 import os
+import shutil
 
-install_script_template = Path(os.path.dirname(os.path.realpath(__file__))) / 'template' / 'install_script.iss'
+
+current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+install_script_template = current_path / 'template' / 'install_script.iss'
+
+installer_executable_path = current_path / 'dist' / 'installer'
 version_info = Version()
 version_info.read_file()
 
+if os.path.exists(installer_executable_path) and os.path.isdir(installer_executable_path):
+    print("removing old installers")
+    shutil.rmtree(installer_executable_path)
+
+print('Making Executable')
+MakeExecutableFile()
+version_info.read_file()
+print('Compiling Installer')
 with open(install_script_template) as f:
     template_text = f.read()
     template_text = template_text.replace(r'{{AppVersion}}', str(version_info))
@@ -17,8 +30,4 @@ with open('install_script_tmp.iss', 'w') as f:
 
 iss_loc = Path.cwd() / 'install_script_tmp.iss'
 compil32_path = Path('C:/Program Files (x86)/Inno Setup 6') / 'iscc'
-
-print('Making Executable')
-MakeExecutableFile()
-print('Compiling Installer')
 subprocess.check_call([compil32_path, '/Qp', str(iss_loc)])
